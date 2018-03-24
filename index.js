f6470afb02f0891ed5254933f548caa320953d76
haykam821 <haykam821@users.noreply.github.com>
2018-03-24 17:00:04 -0400

    One-way gates

--- index.js
+++ index.js
@@ -99,6 +99,31 @@ class LockedWall extends Wall {
   }
 }
 
+const directions = [
+	"north",
+	"east",
+  "south",
+  "west",
+];
+
+class DirectionalWall extends Wall {
+	// this type of wall only collides in one direction
+  constructor(direction, x, y) {
+  	super(x, y);
+    
+    this.color = "#FFEE00";
+  	this.direction = direction > -1 && direction < 4 ? direction : 0;
+  }
+  
+  collides(direction = 2) {
+  	return direction === this.direction;
+  }
+  
+  toString() {
+  	return `One-way gate facing ${directions[this.direction]}`;
+  }
+}
+
 class ItemBox extends Space {
 
 }
@@ -108,14 +133,16 @@ function makeArray(w, h) {
   for (i = 0; i < h; i++) {
     arr[i] = [];
     for (j = 0; j < w; j++) {
-      const random = Math.round(Math.random() * 18);
+      const random = Math.round(Math.random() * 20);
       arr[i][j] = (function() {
         switch (random) {
+          case 0:
+            return new LockedWall(j, i);
           case 1:
           case 2:
             return new Wall(j, i);
-          case 0:
-            return new LockedWall(j, i);
+          case 3:
+          	return new DirectionalWall(Math.round(Math.random() * 4), j, i);
           default:
             return new Space(j, i);
         }
@@ -168,7 +195,7 @@ window.addEventListener("keypress", (event) => {
   switch (event.code) {
     case inputs[0]:
       const tileUp = getTile(curPl.x, curPl.y - 1);
-      if (!tileUp.collides()) {
+      if (!tileUp.collides(2)) {
         curTile.changeBack();
         tileUp.changeTo(curTile);
         players[currentTurn].y--;
@@ -178,7 +205,7 @@ window.addEventListener("keypress", (event) => {
       break;
     case inputs[1]:
       const tileLeft = getTile(curPl.x - 1, curPl.y);
-      if (!tileLeft.collides()) {
+      if (!tileLeft.collides(3)) {
         curTile.changeBack();
         tileLeft.changeTo(curTile);
         players[currentTurn].x--;
@@ -188,7 +215,7 @@ window.addEventListener("keypress", (event) => {
       break;
     case inputs[2]:
       const tileDown = getTile(curPl.x, curPl.y + 1);
-      if (!tileDown.collides()) {
+      if (!tileDown.collides(0)) {
         curTile.changeBack();
         tileDown.changeTo(curTile);
         players[currentTurn].y++;
@@ -198,7 +225,7 @@ window.addEventListener("keypress", (event) => {
       break;
     case inputs[3]:
       const tileRight = getTile(curPl.x + 1, curPl.y);
-      if (!tileRight.collides()) {
+      if (!tileRight.collides(1)) {
         curTile.changeBack();
         tileRight.changeTo(curTile);
         players[currentTurn].x++;
