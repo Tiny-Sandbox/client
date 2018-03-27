797337e7ca746931e986fac6122c73d031a2b595
haykam821 <haykam821@users.noreply.github.com>
2018-03-27 09:25:36 -0400

    Add direction of player to HUD

--- index.js
+++ index.js
@@ -454,7 +454,7 @@ function render() {
     const keys = players[currentTurn].keys;
     hctx.textAlign = "left";
     hctx.textBaseline = "middle";
-    hctx.fillText(`Has ${keys} key${keys === 1 ? "" : "s"}`, hud.width / 8 * 2 + 12, hud.height - 12);
+    hctx.fillText(`Has ${keys} key${keys === 1 ? "" : "s"}, facing ${directions[players[currentTurn].direction]}`, hud.width / 8 * 2 + 12, hud.height - 12);
 
     // AGAIN!
     window.requestAnimationFrame(render);
